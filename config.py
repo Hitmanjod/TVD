5c8409612d591f5ee19916e544f5d4de34895853
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2024-08-19 17:49:56 +0545

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Sujan_BotZ")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "upshrink.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "00904499f65c1e187363a1aad5eb62b4f8fef614")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_To_Verify_Bot/212") # shareus ka tut_vid he 
